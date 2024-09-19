9ba7cc3e9ecdd118a0974f3f20692112d6c9c230
Nessim Gelis <gln5229a@ensfsi.onmicrosoft.com>
2024-09-19 11:06:15 +0200

    ajouter des fichier au rep

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,17 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effectPotion = 1;
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	public String getNom() {
+		return nom;
+	}
+	
+	
+
+}
