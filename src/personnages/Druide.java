d21db472823f8de70399b5cf5a87584832378702
Nessim Gelis <gln5229a@ensfsi.onmicrosoft.com>
2024-09-27 08:27:49 +0200

    chef village duide fini

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -4,8 +4,20 @@ public class Druide {
 	private String nom;
 	private int effetPotionMin;
 	private int effetPotionMax;
-	
-	public Druide(String nom, int effetPotionMin, int effetPotionMin) {
-		this.nom = nom
+	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
+		this.nom = nom;
+		this.effetPotionMin = effetPotionMin;
+		this.effetPotionMax = effetPotionMax;
+		parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller d'une force " + effetPotionMin + " à " + effetPotionMax + ".");
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+	private String prendreParole() {
+		return "Le druide " + nom + " : ";
 	}
 }
+
--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -3,7 +3,8 @@ package personnages;
 public class Gaulois {
 	private String nom;
 	private int force;
-	private int effectPotion = 1;
+	private int effetPotion = 1;
+	
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
@@ -11,32 +12,22 @@ public class Gaulois {
 	public String getNom() {
 		return nom;
 	}
-	
 	public void parler(String texte) {
-		System.out.println(prendreParol() + "« " + texte + "»");
+		System.out.println(prendreParole() + "« " + texte + "»");
 	}
-	
-	private String prendreParol() {
+	private String prendreParole() {
 		return "Le gaulois " + nom + " : ";
 	}
-	
-	
 	public void frapper(Romain romain) {
-		System.out.println(nom + " envoie un grand coup dans la mâchoire de" + romain.getNom());
-		romain.recevoirCoup(force/3);
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());romain.recevoirCoup(force / 3);
 	}
-	
-	
-	
-	@Override
-	public String toString() {
-		return "Gaulois [nom=" + nom + ", force=" + force + ", effectPotion=" + effectPotion + "]";
-	}
-	
+//	@Override
+//	public String toString() {
+//		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+//	}
 	public static void main(String[] args) {
-		//TODO créer un main permettant de tester la classe Gaulois
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		System.out.println(asterix);
 	}
-		
-	}
-
 }
+
