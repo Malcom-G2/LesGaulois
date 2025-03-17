018c2cacf2d0e56438bf22f4d6dc910ecf6296b5
Malcom-G2 <nessim.gelis@univ-tlse3.fr>
2025-03-17 22:34:25 +0100

    fin TP1

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -1,15 +1,39 @@
 package personnages;
+import objets.Chaudron;
 
 public class Druide {
-  private String nom;
-  public String getNom() {
-		return nom;
-	}
-  public void parler(String texte) {
-		System.out.println(prendreParole() + "\"" + texte + "\"");
-	}
-  private String prendreParole() {
-		return "Le Druide " + nom + " : ";
-	}
+    private String nom;
+    private Chaudron chaudron;
 
+    public Druide(String nom) {
+        this.nom = nom;
+        this.chaudron = new Chaudron();
+    }
+
+    public void parler(String texte) {
+        System.out.println(prendreParole() + "\"" + texte + "\"");
+    }
+
+    private String prendreParole() {
+        return "Le Druide " + nom + " : ";
+    }
+
+    public void fabriquerPotion(int quantite, int forcePotion) {
+        chaudron.remplirChaudron(quantite, forcePotion);
+        parler("J'ai concocté " + quantite + " doses de potion magique. Elle a une force de " + forcePotion + ".");
+    }
+
+    public void boosterGaulois(Gaulois gaulois) {
+        if (gaulois.getNom().equals("Obélix")) {
+            parler("Non, Obélix Non !... Et tu le sais très bien !");
+        } else {
+            if (chaudron.resterPotion()) {
+                int effetPotion = chaudron.prendreLouche();
+                gaulois.setEffetPotion(effetPotion);
+                parler("Tiens " + gaulois.getNom() + ", un peu de potion magique.");
+            } else {
+                parler("Désolé, je n'ai plus de potion magique.");
+            }
+        }
+    }
 }
