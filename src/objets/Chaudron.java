b166c1421d3ad187159be32836e269bcb747aea4
Malcom-G2 <nessim.gelis@univ-tlse3.fr>
2025-03-17 22:32:01 +0100

    Update Chaudron.java

--- src/objets/Chaudron.java
+++ src/objets/Chaudron.java
@@ -1,5 +1,29 @@
 package objets;
 
 public class Chaudron {
+    private int quantitePotion;
+    private int forcePotion;
 
+    public Chaudron() {
+        this.quantitePotion = 0;
+        this.forcePotion = 1; // Valeur par défaut
+    }
+
+    public void remplirChaudron(int quantite, int forcePotion) {
+        this.quantitePotion = quantite;
+        this.forcePotion = forcePotion;
+    }
+
+    public boolean resterPotion() {
+        return quantitePotion > 0;
+    }
+
+    public int prendreLouche() {
+        if (quantitePotion > 0) {
+            quantitePotion--;
+            return forcePotion;
+        } else {
+            return 1;
+        }
+    }
 }
