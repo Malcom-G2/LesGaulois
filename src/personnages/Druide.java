db17b1790a81002446389fcb5c3f746df014388f
Malcom-G2 <nessim.gelis@univ-tlse3.fr>
2025-03-17 22:19:58 +0100

    Update Druide.java

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -1,5 +1,15 @@
 package personnages;
 
 public class Druide {
+  private String nom;
+  public String getNom() {
+		return nom;
+	}
+  public void parler(String texte) {
+		System.out.println(prendreParole() + "\"" + texte + "\"");
+	}
+  private String prendreParole() {
+		return "Le Druide " + nom + " : ";
+	}
 
 }
